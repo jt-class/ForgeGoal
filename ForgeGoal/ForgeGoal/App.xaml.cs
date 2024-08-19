f9436996017ddcea139eb08ce40b41fd96542261
TooEzForMe <159233449+JmSaylon@users.noreply.github.com>
2024-08-18 21:59:52 -0800

    initial commit .NET MAUI

---
+++ ForgeGoal/ForgeGoal/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ForgeGoal
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ForgeGoal/ForgeGoal/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ForgeGoal
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
