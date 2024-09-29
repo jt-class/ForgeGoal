8c9509b991abc58e3b994092a81a5eb048b7ce1f
jmbs01 <159233449+JmSaylon@users.noreply.github.com>
2024-09-30 06:44:07 +0800

    Home Page refactor
    Home page refactor

--- ForgeGoal/ForgeGoal/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/ForgeGoal.AssemblyInfo.cs
+++ ForgeGoal/ForgeGoal/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/ForgeGoal.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ForgeGoal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+8ccf5379ed35233a8e23ee648b4f50433f092544")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d71b2516fc12b83ed4e1fa1cf64316bef8bb079a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ForgeGoal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ForgeGoal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
